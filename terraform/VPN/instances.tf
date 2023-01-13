4cfac5b60761e2c9b769a18278f753859bb1dc23
iconx <64724621+iconx2020a@users.noreply.github.com>
2023-01-13 08:26:50 +0100

    Update instances.tf

--- terraform/VPN/instances.tf
+++ terraform/VPN/instances.tf
@@ -1,49 +1,47 @@
-resource "google_compute_instance" "network1_instance" {
-  name         = "network1-instance"
-  machine_type = "e2-micro"
-  zone         = "us-central1-a"
-  tags = ["network1-instance"]
+resource "google_compute_instance" "public_vpn_instance_1" {
+  name         = "public-vpn-instance-1"
+  machine_type = var.machine_type
+  zone         = var.zones[0]
+  tags = ["public-vpn-instance-1"]
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = var.images[0]
       labels = {
-        my_label = "value1"
+        my_label = "publlic-vpn-instance"
       }
     }
   }
   network_interface {
-    network = google_compute_network.network1.name
-    subnetwork = "vpn-subnet-1"
+   network = google_compute_network.public_vpn_network_1.name
+    subnetwork = google_compute_subnetwork.public_vpn_subnet_1.name
     access_config {
-      // Ephemeral public IP
+  // Ephemeral public IP
     }
   }
   metadata = {
-      ssh-keys = "${split("@", data.google_client_openid_userinfo.me.email)[0]}:${tls_private_key.ssh.public_key_openssh}"
+ssh-keys = "${split("@", data.google_client_openid_userinfo.myid.email)[0]}:${tls_private_key.ssh.public_key_openssh}"
   }
 }
-resource "google_compute_instance" "network2_instance" {
-  name         = "network2-instance"
-  machine_type = "e2-micro"
-  zone         = "us-central1-a"
-  tags = ["network2-instance"]
+resource "google_compute_instance" "internal_vpn_instance_1" {
+ provider = google.new-provider
+  name         = "internal-vpn-instance-1"
+  machine_type = var.machine_type
+  zone         = var.zones[1]
+  tags = ["internal-vpn--instance-1"]
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = var.images[0]
       labels = {
-        my_label = "value2"
+        my_label = "inetrnal-vpn-instance-1"
       }
     }
   }
-
   network_interface {
-    network = google_compute_network.network2.name
-    subnetwork = "vpn-subnet-2"
-    access_config {
-      // Ephemeral public IP
-    }
+    network = google_compute_network.internal_vpn_network_1.name
+    subnetwork = google_compute_subnetwork.internal_vpn_subnet_1.name
   }
   metadata = {
   }
   metadata_startup_script = "echo instance 2 of network 2 > /test.txt"
 }
+
