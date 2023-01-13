c2a58a50c07d5cbf8784b38c17bcb11caa02d148
iconx <64724621+iconx2020a@users.noreply.github.com>
2023-01-13 08:25:19 +0100

    Update routers.tf

--- terraform/VPN/routers.tf
+++ terraform/VPN/routers.tf
@@ -1,16 +1,18 @@
 #creating routers
 resource "google_compute_router" "router1" {
   name    = "vpn-router1"
-  network = google_compute_network.network1.name
+  region = var.regions[1]
+  network = google_compute_network.public_vpn_network_1.name
   bgp {
     asn = 64514
   }
 }
-
 resource "google_compute_router" "router2" {
+  provider = google.new-provider
   name    = "vpn-router2"
-  network = google_compute_network.network2.name
+  network = google_compute_network.internal_vpn_network_1.name
   bgp {
     asn = 64515
   }
 }
+
