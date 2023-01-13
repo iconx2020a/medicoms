cb836b0a32fe9fca0e5f08b16de7fed55789e468
iconx <64724621+iconx2020a@users.noreply.github.com>
2023-01-13 08:21:37 +0100

    Update main.tf

--- terraform/VPN/main.tf
+++ terraform/VPN/main.tf
@@ -8,8 +8,15 @@ terraform {
   }
 }
 provider "google" {
-   credentials = file("newkey.json")
-   region = "us-central1"
+  credentials = file("newkey.json")
+  project = var.projects[0]
+  region  = var.regions[0]
+}
+provider "google" {
+  credentials = file("newkey.json")
+  project     = var.projects[1]
+  region      = var.regions[1]
+  alias       = "new-provider"
 }
 provider "google-beta" {
    credentials = file("newkey.json")
@@ -19,17 +26,3 @@ provider "tls" {
   // no config needed
 }
 
-#find user data
-data "google_client_openid_userinfo" "me" {}
-
-#set ssh keys arguments
-resource "tls_private_key" "ssh" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "local_file" "ssh_private_key_pem" {
-  content         = tls_private_key.ssh.private_key_pem
-  filename        = ".ssh/google_compute_engine"
-  file_permission = "0600"
-}
