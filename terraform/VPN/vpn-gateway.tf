4ddb1713dbbf798be026d5c965251c45f62c594f
iconx <64724621+iconx2020a@users.noreply.github.com>
2023-01-13 08:24:42 +0100

    Update vpn-gateway.tf

--- terraform/VPN/vpn-gateway.tf
+++ terraform/VPN/vpn-gateway.tf
@@ -1,13 +1,11 @@
 #creating gateways
 resource "google_compute_ha_vpn_gateway" "gateway1" {
-  region  = "us-central1"
+  region  = var.regions[1]
   name    = "vpn-1"
-  network = google_compute_network.network1.id
+  network = google_compute_network.public_vpn_network_1.id
 }
-
 resource "google_compute_ha_vpn_gateway" "gateway2" {
-  region  = "us-central1"
+ provider = google.new-provider
   name    = "vpn-2"
-  network = google_compute_network.network2.id
+  network = google_compute_network.internal_vpn_network_1.id
 }
-
