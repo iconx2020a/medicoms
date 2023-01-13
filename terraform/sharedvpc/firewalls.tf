88479a690409d2030d91eb3dbc67920ce46a494e
iconx <64724621+iconx2020a@users.noreply.github.com>
2023-01-13 08:45:03 +0100

    Update firewalls.tf

--- terraform/sharedvpc/firewalls.tf
+++ terraform/sharedvpc/firewalls.tf
@@ -8,7 +8,6 @@ resource "google_compute_firewall" "instance_1_ssh_rule" {
   target_tags = ["instance-1"]
   source_ranges = var.ip_ranges
 }
-
 resource "google_compute_firewall" "instance_2_ssh_rule" {
   name = "instance-1-ssh-rule"
   network = google_compute_network.host_network.name
