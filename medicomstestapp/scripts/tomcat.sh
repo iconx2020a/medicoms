8d9ca3e746eceef117f6f282e33418228c0aff33
iconx <64724621+iconx2020a@users.noreply.github.com>
2023-03-03 19:36:43 +0100

    Delete medicomstestapp directory

--- medicomstestapp/scripts/tomcat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt -y install default-jdk
-sudo apt -y install tomcat9 
-sudo service tomcat9 start 
-sudo apt -y install lynx
-sudo apt -y install git
-sudo systemctl enable tomcat9
