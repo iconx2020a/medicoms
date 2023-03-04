9601ac19a62500a28d314c697090897b4e966c9c
iconx <64724621+iconx2020a@users.noreply.github.com>
2023-03-04 09:23:37 +0100

    Update and rename webserver.sh to webserver_centos.sh

--- app/scripts/webserver_centos.sh
+++ app/scripts/webserver_centos.sh
@@ -2,10 +2,8 @@
 #!/bin/bash
 sudo apt update
 sudo apt  -y install httpd
-sudo apt -y  install git
-git clone https://github.com/iconx2020a/medicoms.git
 sudo cp  /etc/httpd/conf.d/vhost.conf  /etc/httpd/conf.d/vhost.conf.bk 
-sudo cp medicoms/000-default.conf /etc/httpd/conf.d/vhost.conf 
+sudo cp 000-default.conf /etc/httpd/conf.d/vhost.conf 
 sudo a2enmod 
 sudo systemctl restart httpd
 sudo systemctl enable httpd 
