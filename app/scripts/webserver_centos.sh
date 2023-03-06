852e005c4146dbd357b5a14bdaf8e80635df245f
iconx <64724621+iconx2020a@users.noreply.github.com>
2023-03-06 05:22:51 +0100

    Update webserver_centos.sh

--- app/scripts/webserver_centos.sh
+++ app/scripts/webserver_centos.sh
@@ -4,6 +4,6 @@ sudo apt update
 sudo apt  -y install httpd
 sudo cp  /etc/httpd/conf.d/vhost.conf  /etc/httpd/conf.d/vhost.conf.bk 
 sudo cp 000-default.conf /etc/httpd/conf.d/vhost.conf 
-sudo a2enmod 
+sudo a2enmod proxy && sudo a2enmod proxy_http && a2enmod ssl
 sudo systemctl restart httpd
 sudo systemctl enable httpd 
